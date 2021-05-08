4c2a427c8d680177bed3934e170f1338e4d920f9
Theodorus Doni <theodorusdoni4@gmail.com>
2021-05-08 20:34:57 +0700

    Fix code coverage CI

--- src/Program.cs
+++ src/Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace Lncodes.Example.Interface
 {
     public class Program
     {
-        static void Main()
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        protected Program() { }
+
+        /// <summary>
+        /// Main Program
+        /// </summary>
+        private static void Main()
         {
-            var attackableCreature = RandomAttackableCreature();
-            var damageableCreature = RandomDamageableCreature();
+            var attackableCreature = CreateAttackableCreatureById(GetRandomAttackableCreatureTypeId());
+            var damageableCreature = CreateDamageableCreatureById(GetRandomDamageableCreatureTypeId());
 
             // Prevent creature from attacking their species
             while (damageableCreature.GetType().Equals(attackableCreature.GetType()))
-                damageableCreature = RandomDamageableCreature();
+                damageableCreature = CreateDamageableCreatureById(GetRandomDamageableCreatureTypeId());
 
             attackableCreature.Attack();
             damageableCreature.TakeDamage(attackableCreature.AttackDamage);
@@ -20,11 +29,12 @@ namespace Lncodes.Example.Interface
         /// <summary>
         /// Method for random damageable creature
         /// </summary>
+        /// <param name="damageableCreatureTypeId"></param>
         /// <returns cref="IDamageable"></returns>
         /// <exception cref="Exception">Thrown when random value > 2</exception>
-        private static IDamageable RandomDamageableCreature()
+        private static IDamageable CreateDamageableCreatureById(int damageableCreatureTypeId)
         {
-            switch (new Random().Next(3))
+            switch (damageableCreatureTypeId)
             {
                 case 0:
                     return new PlayerController();
@@ -33,26 +43,48 @@ namespace Lncodes.Example.Interface
                 case 2:
                     return new NpcController();
                 default:
-                    throw new Exception("Error to random damageable creature");
+                    throw new ArgumentOutOfRangeException(nameof(damageableCreatureTypeId));
             }
         }
 
         /// <summary>
         /// Method for random attackable creature
         /// </summary>
+        /// <param name="attackableCreatureTypeId"></param>
         /// <returns cref="IAttackable"></returns>
         /// <exception cref="Exception">Thrown when random value > 1</exception>
-        private static IAttackable RandomAttackableCreature()
+        private static IAttackable CreateAttackableCreatureById(int attackableCreatureTypeId)
         {
-            switch (new Random().Next(2))
+            switch (attackableCreatureTypeId)
             {
                 case 0:
                     return new PlayerController();
                 case 1:
                     return new EnemyController();
                 default:
-                    throw new Exception("Error to random attackable creature");
+                    throw new ArgumentOutOfRangeException(nameof(attackableCreatureTypeId));
             }
+        }
+
+        /// <summary>
+        /// Method to get random damageable creature id
+        /// </summary>
+        /// <returns cref=int></returns>
+        private static int GetRandomDamageableCreatureTypeId()
+        {
+            var ammountOfDamageableCreatureTypes = 3;
+            return RandomNumberGenerator.GetInt32(0, ammountOfDamageableCreatureTypes);
+        }
+
+
+        /// <summary>
+        /// Method to get random attackable creature id
+        /// </summary>
+        /// <returns cref=int></returns>
+        private static int GetRandomAttackableCreatureTypeId()
+        {
+            var ammountOfAttackableCreatureTypes = 2;
+            return RandomNumberGenerator.GetInt32(0, ammountOfAttackableCreatureTypes);
         }
     }
 }
